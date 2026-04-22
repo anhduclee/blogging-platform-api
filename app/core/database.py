4c494a686af6144b86306b7d89fb998bad0d7d5c
AnhDucLee <anhduclee2006@gmail.com>
2026-04-22 08:44:14 +0000

    Fix async_sessionmaker

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from app.core.config import settings
 from app.models.account import Account
@@ -10,7 +9,7 @@ from app.models.comment import Comment
 
 engine = create_async_engine(url=settings.DB_URL, echo=True, future=True)
 
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def init_db():
     async with engine.begin() as conn:
